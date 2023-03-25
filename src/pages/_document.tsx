ee10cb70077d17302a747cc7e2a54d7f1c3ccb74
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-25 21:05:47 +0530

    Add _document + Update tailwind config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ const Document = () => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <body className="font-inter text-light bg-neutral-800">
+      <body className="font-inter bg-white text-black">
         <Main />
         <NextScript />
       </body>
