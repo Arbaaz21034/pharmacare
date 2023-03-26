ed0c79d365235acba7f6a41167010f667b5b6ad5
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-26 09:02:02 +0530

    Add major changes

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,30 @@
-import Link from "next/link";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const login = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:2003/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+
+      if (!data.success) {
+        setError(data.message);
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="box-border min-w-[360px] rounded-2xl  px-8 py-9 sm:min-w-[440px] sm:px-12 sm:shadow xl:min-w-[480px]">
@@ -11,6 +35,7 @@ const LoginForm = () => {
             className="mt-3 h-10 rounded-lg border border-gray-300 px-2 text-gray-700 focus:outline-primary focus:invalid:outline-red-400"
             required
             autoComplete="on"
+            onChange={(event) => setEmail(event.target.value)}
           />
         </div>
         <div className="mt-8 flex flex-col">
@@ -19,10 +44,14 @@ const LoginForm = () => {
             type="password"
             className="mt-3 h-10 rounded-lg border border-gray-300 px-2 text-gray-700 focus:outline-primary"
             required
+            onChange={(event) => setPassword(event.target.value)}
           />
         </div>
-        <div className="mt-8 h-11 w-full">
-          <button className="flex h-full w-full items-center justify-center rounded-lg bg-primary text-white transition-colors delay-100 ease-in hover:bg-primary-600">
+        <div className="mt-8 h-12 w-full">
+          <button
+            className="flex h-full w-full items-center justify-center rounded-lg bg-primary font-semibold text-white transition-colors delay-100 ease-in hover:bg-primary-400"
+            onClick={(e) => login(e)}
+          >
             Sign in
           </button>
         </div>
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 2003;
 const app = express(2003);
+
+app.use(cors());
+app.use(bodyParser.json());
 
 myDatabase = {
   host: "localhost",
@@ -25,12 +30,47 @@ dbConn.connect((error) => {
 app.get("/api/users", (req, res) => {
   console.log("[GET] /api/users");
   const query = "SELECT * FROM user";
-  dbConn.query(query, (error, results) => {
+  dbConn.query(query, (error, data) => {
+    if (error) {
+      throw error;
+    }
+    res.send(data);
+  });
+});
+
+// embedded query 1
+app.post("/api/login", (req, res) => {
+  console.log("[POST] /api/login");
+  const { email, password } = req.body;
+
+  const query = "SELECT * FROM user";
+  dbConn.query(query, (error, data, field) => {
     if (error) {
       throw error;
     }
 
-    res.send(results);
+    const user = data.find((user) => user.u_email == email);
+
+    if (user) {
+      if (user.u_password == password) {
+        // user exists and has given the correct password
+        console.log(`[+] User ${user.u_id} logged in`);
+        res.send({
+          success: true,
+          message: "Successfully logged in",
+        });
+      } else {
+        res.send({
+          success: false,
+          message: "Incorrect password",
+        });
+      }
+    } else {
+      res.send({
+        success: false,
+        message: "This user does not exist",
+      });
+    }
   });
 });
 
