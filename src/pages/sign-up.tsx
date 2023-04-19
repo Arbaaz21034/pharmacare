3bfebb1fbcea6164453422740e0b44e78b3dd069
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-04-19 23:56:15 +0530

    Update npm packages + Add /sign-up (wip)

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import React from "react";
+
+const SignUp: NextPage = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default SignUp;
